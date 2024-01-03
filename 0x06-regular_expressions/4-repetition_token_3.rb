a43223a9a037b5e8074b809a261634ea64f3594c
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-01-03 21:43:00 +0400

    4. Repetition Token #3

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # This script matches and prints strings that start with 'hb',
-# followed by 0 to 4 't' characters, and ending with 'n'.
+# followed by 0 or more 't' characters, and ending with 'n'.
 
 puts ARGV[0].scan(/hbt*n/).join
 
