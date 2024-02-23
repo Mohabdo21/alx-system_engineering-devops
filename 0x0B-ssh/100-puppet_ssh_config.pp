a87150c5064f582c4dab7fc6c5df21cc5f53d883
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-02-24 03:10:37 +0400

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,9 @@
 file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
   line  => '  IdentityFile ~/.ssh/school',
-  after => 'Host *',
 }
 
 file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
   line  => '  PasswordAuthentication no',
-  after => 'Host *',
 }
