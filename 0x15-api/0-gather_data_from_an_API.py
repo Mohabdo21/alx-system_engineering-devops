a17a4844c480da2abe2271943d734a87faebc72d
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-25 23:24:45 +0400

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+For a given employee ID, returns information about
+their TODO list progress.
+"""
+
+import sys
+
+import requests
+
+
+def employee_todo_progress(employee_id):
+    """Fetches TODO progress for a given employee ID."""
+    user = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    ).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+
+    tasks = [task for task in todos if task["userId"] == int(employee_id)]
+    completed_tasks = [task for task in tasks if task["completed"]]
+
+    print(
+        f"Employee {user['name']} is done "
+        f"with tasks({len(completed_tasks)}/{len(tasks)}):"
+    )
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
+
+
+if __name__ == "__main__":
+    employee_todo_progress(sys.argv[1])
---
+++ 0x15-api/README.md
@@ -0,0 +1,14 @@
+# 0x15. API
+
+This project is part of the Alx - SE curriculum of software engineering. The main objective is to understand and implement the concepts of APIs, microservices, and data formats like CSV and JSON. It also aims to reinforce best practices in Python scripting and naming conventions.
+
+## General Learning Objectives
+
+- Understand what Bash scripting should not be used for.
+- Learn what an API is and how to use it.
+- Understand what a REST API is and how it works.
+- Learn about microservices and their use cases.
+- Understand the CSV format and its applications.
+- Learn about the JSON format and how to use it.
+- Understand Pythonic naming conventions for packages, modules, classes, variables, functions, and constants.
+- Learn about the significance of CapWords or CamelCase in Python.
