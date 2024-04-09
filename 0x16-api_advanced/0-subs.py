830b287e816d0e11042eff65846752ac5bc0a941
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-04-10 01:12:01 +0400

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,14 +14,16 @@ def number_of_subscribers(subreddit):
     """
     Fethes the total number of subscribers for a given subreddit
     """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": (
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) "
             "Gecko/20100101 Firefox/124.0"
             )
     }
+
     response = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        url,
         headers=headers,
         allow_redirects=False,
         timeout=10
