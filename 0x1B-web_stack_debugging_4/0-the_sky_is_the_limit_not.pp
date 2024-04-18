abca594526b5224bd85b23f4aaede36fab433a6e
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-04-18 23:59:53 +0400

    CleanUp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,13 @@
-#0-the_sky_is_the_limit_not.pp level up open file limit
-exec { 'set open file limit to 4096':
+# File: 0-the_sky_is_the_limit_not.pp
+# Purpose: Increase the open file limit for the nginx
+
+# Level up open file limit
+exec { 'set-limit-to-4096':
   path    => '/bin',
   command => "sed -i 's/15/4096/' /etc/default/nginx"
 }
+
+# Reboot nginx
 exec { 'reboot nginx':
   command => '/usr/sbin/service nginx restart'
 }
